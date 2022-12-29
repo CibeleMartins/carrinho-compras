35bc33204ea1f2252cb7d3e2db517b99ff23ad48
Cibele Martins <Cibele.Martins@ibm.com>
2022-12-29 13:04:51 -0300

    🚧 feat: começou implementar a Home e criou a pasta do componente de animação

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import { HStack } from "@chakra-ui/react"
+
+const Home = ()=> {
+
+    return (
+        <HStack
+        w="100%"
+        h="100vh"
+        bg="#FFFFF0">
+
+        </HStack>
+    )
+}
+
+export default Home;
