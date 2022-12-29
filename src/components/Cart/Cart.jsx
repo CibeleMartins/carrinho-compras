d125b291a4a7cb69805bfc3a37e73220154d382b
Cibele Martins <Cibele.Martins@ibm.com>
2022-12-29 16:35:54 -0300

    🚧 feat: criou um modal e um componente de carrinho

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,16 @@
+import { HStack } from '@chakra-ui/react';
+
+import Modal from './Modal/Modal'
+
+const Cart = ()=> {
+
+    return (
+        <Modal>
+            <HStack>
+                
+            </HStack>
+        </Modal>
+    )
+}
+
+export default Cart;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,12 @@ import { HStack } from '@chakra-ui/react';
 
 import ShoppingCartLottie from '../components/LottieAnimation/ShoppingCartLottie';
 import ProductLists from '../components/ProductsList/ProductsList';
+import Cart from '../components/Cart/Cart';
 
 const Home = () => {
   return (
     <HStack w="100%" h="100vh" bg="#FFFFF0" spacing="10%">
+      <Cart/>
       <ProductLists/>
       <ShoppingCartLottie />
     </HStack>
