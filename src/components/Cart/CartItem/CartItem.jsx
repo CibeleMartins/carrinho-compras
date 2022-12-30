ee3314fbf98d701802357dfc3fc986d11a5b5877
Cibele Martins <Cibele.Martins@ibm.com>
2022-12-30 10:00:19 -0300

    💄 feat: adicionou estilo no carrinho e nos produtos do carrinho

--- src/components/Cart/CartItem/CartItem.jsx
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -1,10 +1,16 @@
 import { HStack, Text } from '@chakra-ui/react';
 
-
 const CartItem = ({ productName, productAmount, productPrice }) => {
-  
   return (
-    <HStack>
+    <HStack
+      h="10vh"
+      w="80%"
+      bg="white"
+      pl="5%"
+      pr="5%"
+      borderRadius={10}
+      boxShadow="0 2px 8px rgba(0, 0, 0, 0.25)"
+    >
       <Text>{productName}</Text>
       <Text>{productAmount}</Text>
       <Text>{productPrice}</Text>
